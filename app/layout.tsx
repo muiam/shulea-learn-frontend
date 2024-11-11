026ed8b28e8920a9d0e71a09764454edbeda1d0c
muiam <mwendwaharry01@gmail.com>
2024-11-11 12:52:21 +0300

    added meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { NextUIProvider } from "@nextui-org/react";
 import { UserProvider } from "./learner/(userContext)/userContext";
 import { Nunito } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
+import Head from "next/head";
 
 const myAppFont = Nunito({ subsets: ["latin"] });
 
@@ -22,6 +23,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        {/* Optional: Twitter card meta tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="shulea learn - Teach, Learn, Earn!"
+        />
+        <meta
+          name="twitter:description"
+          content="shulea learn is learning platform that enables tutors create lessons , enroll learners easily , receive enrollment fees into their wallets and offer training with resource sharing all under one roof."
+        />
+        <meta
+          name="twitter:image"
+          content="https://shulea-learn-frontend.vercel.app/logo.svg"
+        />
+      </Head>
       <body className={myAppFont.className}>
         <NextUIProvider>
           <UserProvider>
