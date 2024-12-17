818537222578cdcb0c9a334e1dd4d8fd713b5b2a
muiam <mwendwaharry01@gmail.com>
2024-12-17 23:54:41 +0300

    details

--- app/api/lesson/[lessonId]/details/route.ts
+++ app/api/lesson/[lessonId]/details/route.ts
@@ -3,23 +3,25 @@ import { client } from "@/sanity/lib/client";
 import { lessonDetailsQuery } from "@/sanity/lib/queries";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-  const { lessonId } = req.query; // Destructure `lessonId` from `params`
-  if(!lessonId){
-    return res.status(400).json({ error: "No lessonId found" });
-  }
-  try {
-    const details = await client.fetch(lessonDetailsQuery(lessonId as string));
-    if (details.length === 0) {
-      return NextResponse.json({ error: "No details found" }, { status: 404 });
-    }
+export async function GET(req: NextRequest, res: NextApiResponse) {
+  // const { lessonId } = req.query; // Destructure `lessonId` from `params`
+  // if(!lessonId){
+  //   return res.status(400).json({ error: "No lessonId found" });
+  // }
+  // try {
+  //   const details = await client.fetch(lessonDetailsQuery(lessonId as string));
+  //   if (details.length === 0) {
+  //     return NextResponse.json({ error: "No details found" }, { status: 404 });
+  //   }
 
-    return NextResponse.json(details[0], { status: 200 });
-  } catch (error: unknown) {
-    console.error("Error fetching goals:", error);
-    if (error instanceof Error) {
-      return NextResponse.json({ error: "Internal Server Error", details: error.message }, { status: 500 });
-    }
-    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
-  }
+  //   return NextResponse.json(details[0], { status: 200 });
+  // } catch (error: unknown) {
+  //   console.error("Error fetching goals:", error);
+  //   if (error instanceof Error) {
+  //     return NextResponse.json({ error: "Internal Server Error", details: error.message }, { status: 500 });
+  //   }
+  //   return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  // }
+
+  return NextResponse.json("hello world");
 }
