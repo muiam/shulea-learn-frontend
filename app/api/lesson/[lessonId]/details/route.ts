5bc90ac2ac9dbef454abf469678c2f2666a4d564
muiam <mwendwaharry01@gmail.com>
2024-12-17 23:30:41 +0300

    commented off lesson details route

--- app/api/lesson/[lessonId]/details/route.ts
+++ app/api/lesson/[lessonId]/details/route.ts
@@ -1,24 +1,22 @@
-import { NextRequest, NextResponse } from "next/server";
-import { client } from "@/sanity/lib/client";
-import { lessonDetailsQuery } from "@/sanity/lib/queries";
+// import { NextRequest, NextResponse } from "next/server";
+// import { client } from "@/sanity/lib/client";
+// import { lessonDetailsQuery } from "@/sanity/lib/queries";
+// import { NextApiRequest } from "next";
 
-export async function GET(request: NextRequest, context: { params: { lessonId: string } }) {
-  const { lessonId } = context.params; // Destructure `lessonId` from `params`
+// export async function handler(req: NextApiRequest, res: NextApiRequest) {
+//   const { lessonId } = req.query; // Destructure `lessonId` from `params`
+//   try {
+//     const details = await client.fetch(lessonDetailsQuery(lessonId));
+//     if (details.length === 0) {
+//       return NextResponse.json({ error: "No details found" }, { status: 404 });
+//     }
 
-  console.log(`Endpoint triggered, and lessonId is ${lessonId}`);
-
-  try {
-    const details = await client.fetch(lessonDetailsQuery(lessonId));
-    if (details.length === 0) {
-      return NextResponse.json({ error: "No details found" }, { status: 404 });
-    }
-
-    return NextResponse.json(details[0], { status: 200 });
-  } catch (error: unknown) {
-    console.error("Error fetching goals:", error);
-    if (error instanceof Error) {
-      return NextResponse.json({ error: "Internal Server Error", details: error.message }, { status: 500 });
-    }
-    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
-  }
-}
+//     return NextResponse.json(details[0], { status: 200 });
+//   } catch (error: unknown) {
+//     console.error("Error fetching goals:", error);
+//     if (error instanceof Error) {
+//       return NextResponse.json({ error: "Internal Server Error", details: error.message }, { status: 500 });
+//     }
+//     return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+//   }
+// }
