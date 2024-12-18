79d0952ec5e4a39edb3aa420bce18411a3ee0f26
muiam <mwendwaharry01@gmail.com>
2024-12-18 11:58:37 +0300

    refined env variables

--- sanity/env.ts
+++ sanity/env.ts
@@ -8,8 +8,8 @@ export const dataset = assertValue(
 
 export const token = assertValue(
   
-"skEObKiTr5eZtOcwxXyOK4Bw4vjQRmhGI4AUO5OnpOJuUirYfGr5t0hFy66w6lGHz7Dn6CSeQLGDv7Na9LLh0UodXmAdx3OBdB1ljRozVBxLmWReFc8XdSFU9nUiToCK4V1yx75mWM6A5R5pXVxPXVpyCuA1IqiMoO1NxzfKACLH4h7YY4Nj",
-  'Missing environment variable: SANITY_TOKEN'
+  process.env.SANITY_TOKEN,
+'Missing environment variable: SANITY_TOKEN'
 )
 
 export const projectId = assertValue(
@@ -17,7 +17,7 @@ export const projectId = assertValue(
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 export const readToken = assertValue(
-  "skxinLB8y94jyaSHHIRXuXQHKGgMy6Dj2SxYguPxs2ujR88Axz5vmqpp5Jfgn2kHopHkhAl2OCOUpwG5u3QKFMFsn1i6Svep99PBVPG4rMzAnPlWFVQBUvEdBUaKClxjyArKIbQ6bT4ga04c3RRfY5RbBoEiEyQWAvsIxdRSmwI5WtbdyNDn",
+  process.env.SANITY_READ_TOKEN,
   'Missing environment variable: SANITY_READ_TOKEN'
 )
 
